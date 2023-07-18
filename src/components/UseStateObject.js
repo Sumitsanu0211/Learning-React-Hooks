5f5058d0638428ae1fda70d91198ea175aa7c48c
Sumit Kumar <sumitsanu19@gmail.com>
2023-07-18 15:19:10 +0530

    Learning about spread operator

---
+++ src/components/UseStateObject.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UseStateObject = () => {
+  return (
+    <>
+      <h1>Welcome to objects and understands three dots.</h1>
+    </>
+  )
+}
+
+export default UseStateObject
