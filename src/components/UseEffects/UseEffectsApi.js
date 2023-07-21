237bdbf4a9b6d22447f516a837f02b889b6a5683
Sumit Kumar <sumitsanu19@gmail.com>
2023-07-21 14:09:28 +0530

    use effect and ApI fetching for getting data

--- src/components/UseEffects/UseEffectsApi.js
+++ src/components/UseEffects/UseEffectsApi.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const UseEffectsApi = () => {
+//API fetching
+      const getUsers = async ()=>{
+            const response = await fetch('https://api.github.com/users');
+            const data = await response.json();
+            console.log(data);
+
+      }
+
+      useEffect( () => {
+            getUsers();
+      })
   return (
     <>
       <h2>List of Github Users</h2>
+      <div className="container-fluid mt-5">
+            <div className="row text center">
+                  <div className="col-10 col-md-4 mt-5"></div>
+
+            </div>
+      </div>
     </>
   )
 }
